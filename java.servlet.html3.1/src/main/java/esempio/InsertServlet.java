dfbe5e701aeb1344867f2a09e96c90d21c5b68a7
DaimCod <daim.kazmi00@gmail.com>
2023-01-12 16:31:47 +0100

    Correzione maurizio

--- java.servlet.html3.1/src/main/java/esempio/InsertServlet.java
+++ java.servlet.html3.1/src/main/java/esempio/InsertServlet.java
@@ -8,16 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class DeleteServlet
+ * Servlet implementation class InsertServlet
  */
-@WebServlet("/DeleteID")
-public class DeleteServlet extends HttpServlet {
+@WebServlet("/Insert")
+public class InsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DeleteServlet() {
+    public InsertServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,38 +26,38 @@ public class DeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 //deve coincidere con il nome dei parametri in html
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email"); //deve coincidere con il nome dei parametri in html
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String birthday = request.getParameter("birthday");
+		String imgpath = request.getParameter("imgpath");
+		String role = request.getParameter("role");
+		String hrnote = request.getParameter("hrnote");
+		String technicalnote = request.getParameter("technicalnote");
+		String enabled = request.getParameter("enabled");
 		
-		response.getWriter().append("<html><body><h2>Scegliere l'id tra queste opzioni</h2><br>");
-				
+		response.getWriter().append("<html><body>Ho ricevuto correttamente i dati<br>");
 		
 		String finalResult = "";
 		
 		try {
-			response.getWriter().append(DatabaseManagementSingleton.getIstance()
-			   						   .retriveFromDB());
-			response.getWriter().append( 
-					 "		<form>"
-					+ "			  <label>ID da cancellare: </label>"
-					+ "			  <input type=\"text\" name=\"id\"><br><br>"
-					+ "			  <input type=\"submit\" value=\"Cancella ora\">"
-					+ "		</form>");
-			String id = request.getParameter("id");
-			DatabaseManagementSingleton.getIstance()
-									   .deleteFromDB(id);
 			
+			int result = DatabaseManagementSingleton.getIstance()
+									.insertIntoDB(email, firstName, lastName, birthday, imgpath,role,hrnote,technicalnote,enabled);
 			
+			response.getWriter().append("Ho ricevuto correttamente i dati<br>");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		//response.getWriter().append("<a href=\"./List\"> Clicca qui per visualizzare il cambiamento</a>");
 		
-		 
 		
-		response.getWriter().append("<a href=\"./List\"> Visualizza aggiornamento </a>");
+		response.getWriter().append("<a href=\"./List\"> Clicca qui per visualizzare </a>");
+		
 		finalResult += "</body></html>";
+		
 		response.getWriter().append(finalResult);
 	}
 
